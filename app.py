28e1e8f07f129865fa492b13ccc88ba7e73163f3
unknown <bhekinkosipraisem@gmail.com>
2025-11-08 17:42:39 +0200

    updated for github pages

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+# This Flask app has been converted to a static site for GitHub Pages deployment
+# The main file is now index.html in the root directory
+# To run locally with Flask, uncomment the code below:
 
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template("Index.html")
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# from flask import Flask, render_template
+# app = Flask(__name__)
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+# if __name__ == '__main__':
+#     app.run(debug=True)
